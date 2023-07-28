f52256ec446cc90d73d4fdab16885e3f31142e36
geeseanonymous <66060314+geeseanonymous@users.noreply.github.com>
2023-07-29 00:23:52 +0800

    Add files via upload

--- chars/init.js
+++ chars/init.js
@@ -5,7 +5,7 @@ function init() {
     header.innerHTML = `
     <nav class="navbar navbar-expand-md navbar-dark fixed-top">
     <div class="container-fluid">
-    <a class="navbar-brand" href="../index.html">gay archive</a>
+    <a class="navbar-brand" href="../index.html">archive archive</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
         aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
